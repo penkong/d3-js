386a36da5af7c91d3abfae7b9b43bc47da05f538
mk-nazemi <nazemi.works@gmail.com>
2019-09-29 10:02:58 +0330

    male d3.js scales

--- 2/js/main.js
+++ 2/js/main.js
@@ -49,35 +49,144 @@
 // can do request to out data source.
 
 // bring in data from out source
+// d3.json('data/buildings.json').then(data => {
+//     // console.log(data);
+//     // turn data to integer by this trick
+//     data.forEach(d => d.height = +d.height);
+//     // make svg
+//     let svg = d3
+//         .select('#chart-area')
+//         .append('svg')
+//         .attr('width', 400)
+//         .attr('height', 400);
+
+//     // define shapes
+//     let circles = svg
+//         .selectAll('circle').data(data);
+
+
+//     // add attrs .
+//     circles
+//         .enter()
+//         .append('circle')
+//         .attr('cx', (d, i) => {
+//             return (i * 70) + 45;
+//         })
+//         .attr('cy', 45)
+//         .attr('r', (d) => {
+//             return d.height / 10;
+//         })
+//         .attr('fill', d => {
+//             if (d.name == "Shanghai Tower") return 'blue'
+//             else return 'red';
+//         });
+// }).catch(err => console.log(err));
+
+
+// d3.json('data/buildings.json').then(data => {
+//     data.forEach(d => d.height = +d.height);
+//     let svg = d3
+//         .select('#chart-area')
+//         .append('svg')
+//         .attr('width', 400)
+//         .attr('height', 400);
+
+//     let circles = svg
+//         .selectAll('rect').data(data);
+
+
+//     circles
+//         .enter()
+//         .append('rect')
+//         .attr('x', (d, i) => {
+//             return (i * 70) + 45;
+//         })
+//         .attr('y', 20)
+//         .attr('height', (d) => {
+//             return d.height / 2;
+//         })
+//         .attr('width', 30)
+//         .attr('fill', d => {
+//             if (d.name == "Shanghai Tower") return 'blue'
+//             else return 'grey';
+//         });
+// }).catch(err => console.log(err));
+//-----------------------------------------------------------------------
+// because size of svg.
+// scales are funcs that map from an input domain to an output range
+// base on min and max
+// const y = d3.scaleLinear().domain([0, 828]).range([0, 200]); 
+// clg(y(100)) 48.8         clg(y.invert(48.8))
+
+// let svg = d3
+//     .select('#chart-area')
+//     .append('svg')
+//     .attr('width', 400)
+//     .attr('height', 400);
+
+// d3.json('data/buildings.json').then(data => {
+
+//     data.forEach(d => d.height = +d.height);
+
+//     let y = d3.scaleLinear()
+//         .domain([0, 350])
+//         .range([0, 300]);
+
+//     let rects = svg
+//         .selectAll('rect')
+//         .data(data)
+//         .enter()
+//         .append('rect')
+//         .attr('x', (d, i) => {
+//             return (i * 70) + 20;
+//         })
+//         .attr('y', 40)
+//         .attr('height', (d) => {
+//             return y(d.height);
+//         })
+//         .attr('width', 30)
+//         .attr('fill', d => {
+//             if (d.name == "Shanghai Tower") return 'blue'
+//             else return 'grey';
+//         });
+// }).catch(err => console.log(err));
+
+
+//------------------------------------------------
+// logarithm scales.
+// usage like population scale
+// min max and base 
+// base 1 is linear 
+// it must negative or positive not zero for log undefined.
+let svg = d3
+    .select('#chart-area')
+    .append('svg')
+    .attr('width', 400)
+    .attr('height', 400);
+
 d3.json('data/buildings.json').then(data => {
-    // console.log(data);
-    // turn data to integer by this trick
+
     data.forEach(d => d.height = +d.height);
-    // make svg
-    let svg = d3
-        .select('#chart-area')
-        .append('svg')
-        .attr('width', 400)
-        .attr('height', 400);
 
-    // define shapes
-    let circles = svg
-        .selectAll('circle').data(data);
+    let y = d3.scaleLinear()
+        .domain([0, 350])
+        .range([0, 300]);
 
-
-    // add attrs .
-    circles
+    let rects = svg
+        .selectAll('rect')
+        .data(data)
         .enter()
-        .append('circle')
-        .attr('cx', (d, i) => {
-            return (i * 70) + 45;
+        .append('rect')
+        .attr('x', (d, i) => {
+            return (i * 70) + 20;
         })
-        .attr('cy', 45)
-        .attr('r', (d) => {
-            return d.height / 10;
+        .attr('y', 40)
+        .attr('height', (d) => {
+            return y(d.height);
         })
+        .attr('width', 30)
         .attr('fill', d => {
             if (d.name == "Shanghai Tower") return 'blue'
-            else return 'red';
+            else return 'grey';
         });
 }).catch(err => console.log(err));
