ae21482419364f8d46664c02ead371ae2479f7bd
mk-nazemi <nazemi.works@gmail.com>
2019-09-29 15:02:31 +0330

    add some code

---
+++ 4/index.js
@@ -0,0 +1,28 @@
+// tufte design 
+// easy interpret credible visualizations
+// keep focus on important focus.
+// lie factor  by tufte
+// bad practice 3d and background
+// data to ink ratio => simple high data / low ink
+// graphical integrity
+
+// the lie factor
+
+// chart junk
+
+// data - to - ink ratio
+
+// eliminate distractions .
+
+// jacques berins visual desing space
+// position size value texture color orientation shape
+// data => quantitative (numerical) categorical (categories) ordinal (rankings);
+// 5- 8 colors
+
+// colorbrewer2.org
+
+// using interval to continue update on chaanege
+d3.interval(() => {}, 400);
+setInterval();
+clearInterval();
+//  write at end;
